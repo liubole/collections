45e6128024a3af466016ee5168f7b58f3c1a0c67
liubole <liubole@xiaozhu.com>
2019-01-21 14:21:48 +0800

    getScriptParam

--- script/functions.php
+++ script/functions.php
@@ -18,3 +18,22 @@ if (!function_exists('getScriptParams')) {
         return $matches;
     }
 }
+
+/**
+ * Get script param
+ * var_dump(getScriptParam(["--startTime='2018-01-01 00:00:00'", "--endTime='2018-01-21 00:00:00'", "qaz=wsx"], 'startTime', '--'));
+ */
+if (!function_exists('getScriptParam')) {
+    function getScriptParam($params, $name, $prefix = '--')
+    {
+        if (is_array($params)) {
+            foreach ($params as $param) {
+                if (substr_compare($param, $prefix, 0, strlen($prefix)) !== 0) continue;
+                $arr = [];
+                parse_str(substr($param, strlen($prefix)), $arr);
+                if (isset($arr[$name])) return $arr[$name];
+            }
+        }
+        return NULL;
+    }
+}
